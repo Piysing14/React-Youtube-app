4b14a28482aac217ccb7032d267db5b5667c444e
Piyush Singh <khatipk14@gmail.com>
2024-09-29 20:15:31 +0530

    adding initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name='react app'
+  console.log("App");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        {name}
+        <Demo></Demo>
+      </div>
+    </div>
+  );
+}
+
+function Demo() {
+  console.log("Demo");
+
+  return (
+    <div className="Demo">
+      <div className='Demo-header'>
+        Demo
+      </div>
     </div>
   );
 }
